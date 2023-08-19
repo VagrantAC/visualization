b763c575cb708ef7ea2c991fd1a775aa2547c13d
VagrantAC <vagrantac@126.com>
2023-08-19 17:45:20 +0800

    rectangle

--- webGL/src/main.ts
+++ webGL/src/main.ts
@@ -37,6 +37,7 @@ function main() {
   const program = createProgram(gl, vertexShader, fragmentShader)!;
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
+  const resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
 
   // Create a buffer and put three 2d clip space points in it
   const positionBuffer = gl.createBuffer();
@@ -44,11 +45,15 @@ function main() {
   // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  const positions = [
-    0, 1,
-    1, 0,
-    1, 1
+  var positions = [
+    10, 20,
+    80, 20,
+    10, 30,
+    10, 30,
+    80, 20,
+    80, 30,
   ];
+
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
   gl.canvas.width = window.innerWidth;
   gl.canvas.height = window.innerHeight;
@@ -69,6 +74,8 @@ function main() {
   // Bind the position buffer.
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
   // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
   const size = 2;          // 2 components per iteration
   const type = gl.FLOAT;   // the data is 32bit floats
@@ -81,7 +88,7 @@ function main() {
   // draw
   const primitiveType = gl.TRIANGLES;
   const arraysOffset = 0;
-  const count = 3;
+  const count = 6;
   gl.drawArrays(primitiveType, arraysOffset, count);
 }
 
