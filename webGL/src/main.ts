1ea925ff5226822b92b367297fcbdf8775d00d8e
VagrantAC <vagrantac@VagrantACdeAir.lan>
2023-09-10 23:21:00 +0800

    (feature) 2D support translation and rotation

--- webGL/src/main.ts
+++ webGL/src/main.ts
@@ -1,69 +1,29 @@
 import { createProgramFromScripts, resizeCanvasToDisplaySize } from "twgl.js";
-import { setFramebuffer, setRectangle } from "./utils/set";
-import { requestCORSIfNotSameOrigin } from "./utils/request";
-import { createAndSetupTexture } from "./utils/texture";
-import { drawWithKernel, kernels } from "./const";
-
+import { setGeometry } from "./utils/set";
 
 function main() {
-  const image = new Image();
-  requestCORSIfNotSameOrigin(image, "https://webglfundamentals.org/webgl/resources/leaves.jpg")
-  image.src = "https://webglfundamentals.org/webgl/resources/leaves.jpg"
-  image.onload = () => {
-    render(image)
-  }
-}
-
-function render(image: HTMLImageElement) {
   const canvas = document.querySelector("#root") as HTMLCanvasElement;
   const gl = canvas.getContext("webgl")!;
+  gl.canvas.width = 1000;
+  gl.canvas.height = 1000;
 
   const program = createProgramFromScripts(gl, ["vertex-shader-2d", "fragment-shader-2d"]);
   const positionLocation = gl.getAttribLocation(program, "a_position");
-  const texcoordLocation = gl.getAttribLocation(program, "a_texCoord");
+  const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  const colorLocation = gl.getUniformLocation(program, "u_color");
+  const translationLocation = gl.getUniformLocation(program, "u_translation");
+  const rotationLocation = gl.getUniformLocation(program, "u_rotation");
 
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  setRectangle(gl, 0, 0, image.width, image.height);
 
-  const texcoordBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    0.0, 0.0,
-    1.0, 0.0,
-    0.0, 1.0,
-    0.0, 1.0,
-    1.0, 0.0,
-    1.0, 1.0,
-  ]), gl.STATIC_DRAW);
-
-
-  const originalImageTexture = createAndSetupTexture(gl);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-
-  const textures: WebGLTexture[] = [];
-  const framebuffers: WebGLFramebuffer[] = [];
-  for (let i = 0; i < 2; i++) {
-    const texture = createAndSetupTexture(gl);
-    textures.push(texture!);
-
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, image.width, image.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-
-    const fbo = gl.createFramebuffer();
-    framebuffers.push(fbo!);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
-
-    gl.framebufferTexture2D(
-      gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
-  }
-  const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
-  const textureSizeLocation = gl.getUniformLocation(program, "u_textureSize");
-  const kernelLocation = gl.getUniformLocation(program, "u_kernel[0]");
-  const kernelWeightLocation = gl.getUniformLocation(program, "u_kernelWeight");
-  const flipYLocation = gl.getUniformLocation(program, "u_flipY");
+  const translation = [0, 200];
+  const rotation = [1, 0];
+  const color = [Math.random(), Math.random(), Math.random(), 1];
 
   resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
-  gl.clearColor(0, 0, 0, 0);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.useProgram(program);
@@ -71,6 +31,7 @@ function render(image: HTMLImageElement) {
   gl.enableVertexAttribArray(positionLocation);
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
+  setGeometry(gl, translation[0], translation[1], 100, 150, 30);
   const positionAttribute = {
     size: 2,
     type: gl.FLOAT,
@@ -81,32 +42,15 @@ function render(image: HTMLImageElement) {
   gl.vertexAttribPointer(
     positionLocation, positionAttribute.size, positionAttribute.type, positionAttribute.normalize, positionAttribute.stride, positionAttribute.offset);
 
-  gl.enableVertexAttribArray(texcoordLocation);
-  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-  const texcoordAttribute = {
-    size: 2,
-    type: gl.FLOAT,
-    normalize: false,
-    stride: 0,
-    offset: 0,
-  }
-  gl.vertexAttribPointer(
-    texcoordLocation, texcoordAttribute.size, texcoordAttribute.type, texcoordAttribute.normalize, texcoordAttribute.stride, texcoordAttribute.offset);
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+  gl.uniform4fv(colorLocation, color);
+  gl.uniform2fv(translationLocation, translation);
+  gl.uniform2fv(rotationLocation, rotation);
 
-  gl.uniform2f(textureSizeLocation, image.width, image.height);
-  gl.bindTexture(gl.TEXTURE_2D, originalImageTexture);
-  gl.uniform1f(flipYLocation, 1);
-
-
-  [kernels.gaussianBlur2, kernels.emboss, kernels.boxBlur, kernels.sharpness, kernels.previtVertical].forEach((kernel, count) => {
-    setFramebuffer(gl, resolutionLocation, framebuffers[count % 2], image.width, image.height);
-    drawWithKernel(gl, kernelLocation!, kernelWeightLocation!, kernel);
-    gl.bindTexture(gl.TEXTURE_2D, textures[count % 2]);
-  })
-
-  gl.uniform1f(flipYLocation, -1);  // need to y flip for canvas
-  setFramebuffer(gl, resolutionLocation, null, gl.canvas.width, gl.canvas.height);
-  drawWithKernel(gl, kernelLocation!, kernelWeightLocation!, kernels.normal);
+  const primitiveType = gl.TRIANGLES;
+  const offset = 0;
+  const count = 18;
+  gl.drawArrays(primitiveType, offset, count);
 }
 
 main();
--- webGL/src/utils/set.ts
+++ webGL/src/utils/set.ts
@@ -9,6 +9,34 @@ export function setRectangle(gl: WebGLRenderingContext, x: number, y: number, wi
   ]), gl.STATIC_DRAW);
 }
 
+export function setGeometry(gl: WebGLRenderingContext, x: number, y: number, width: number, height: number, thickness: number) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      x, y,
+      x + thickness, y,
+      x, y + height,
+      x, y + height,
+      x + thickness, y,
+      x + thickness, y + height,
+
+      x + thickness, y,
+      x + width, y,
+      x + thickness, y + thickness,
+      x + thickness, y + thickness,
+      x + width, y,
+      x + width, y + thickness,
+
+      x + thickness, y + thickness * 2,
+      x + width * 2 / 3, y + thickness * 2,
+      x + thickness, y + thickness * 3,
+      x + thickness, y + thickness * 3,
+      x + width * 2 / 3, y + thickness * 2,
+      x + width * 2 / 3, y + thickness * 3,
+    ]),
+    gl.STATIC_DRAW);
+}
+
 export function setFramebuffer(gl: WebGLRenderingContext, resolutionLocation: WebGLUniformLocation | null, fbo: WebGLFramebuffer | null, width: number, height: number) {
   gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
   gl.uniform2f(resolutionLocation, width, height);
